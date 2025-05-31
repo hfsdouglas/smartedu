1bdb621050bd42644b929b7693d79cfcc6b910d1
Douglas Faria <hfsdouglas@gmail.com>
2025-05-30 22:54:48 -0300

    feat: criar cadastro de novos usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
-{
+{   
     public function login(Request $request) {
         $request->validate([
             'email' => 'required|email',
@@ -27,5 +27,27 @@ class UserController extends Controller
         }
 
         return response()->json(['message' => 'E-mail ou senha invalidos!'], 401);
+    }
+
+    public function signup(Request $request) {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:5',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => "Olá, $user->name! Seu cadastro foi efetuado com sucesso.",
+            'token' => $token,
+            'user' => $user
+        ]);
     }
 }
