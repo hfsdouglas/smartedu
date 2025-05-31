8a53abedf11bf590ac0914ebcb526cb78a36f861
Douglas Faria <hfsdouglas@gmail.com>
2025-05-30 22:51:28 -0300

    feat: cria funcao de login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,5 +7,25 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            $user = auth()->user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => "Olá, $user->name! Seja bem-vindo(a) ao Smartedu.",
+                'token' => $token,
+                'user' => $user
+            ]);
+        }
+
+        return response()->json(['message' => 'E-mail ou senha invalidos!'], 401);
+    }
 }
