dbf205ff2a5647246fbcf579c2d5eb4e686e9a8f
Douglas Faria <hfsdouglas@gmail.com>
2025-05-30 23:56:34 -0300

    fix: redireciona a rota para index

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/user')->group(function () {
-    Route::get('/', [UserController::class])->middleware('auth:sanctum');
+    Route::get('/', [UserController::class, 'index'])->middleware('auth:sanctum');
     Route::get('/{id}', [UserController::class, 'show'])->middleware('auth:sanctum');
 });
 
