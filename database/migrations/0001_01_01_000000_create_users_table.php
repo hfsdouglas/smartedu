d48a1702e22b25c65243b9764f1ea1701d6af73a
Douglas Faria <hfsdouglas@gmail.com>
2025-06-11 21:38:48 -0300

    feat: add campos cpf, email, celular

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('cpf')->unique();
+            $table->string('celular')->unique();
+            $table->enum('ativo', ['S', 'N'])->default('S');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
