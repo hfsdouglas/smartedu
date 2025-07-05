c2b9bd55eb09669ea5213da90bfb898f64ead5df
Douglas Faria <hfsdouglas@gmail.com>
2025-07-04 22:46:52 -0300

    feat: rota login

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,12 +2,23 @@ import { createBrowserRouter } from "react-router";
 
 import { AppLayout } from "./pages/_layouts/app";
 import { ErrorPage } from "./pages/error";
+import Dashboard from "./pages/dashboard";
+import Login from "./pages/login";
 
 export const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        element: <Dashboard />,
         path: "/",
-        element: <AppLayout />,
-        errorElement: <ErrorPage />,
-        children: []
-    }
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 ]);
